36ef8e14341afd4597e2b5bba7feb613b80195c0
Anna Anecka <anna.eliseeva1997.eliseeva@mail.ru>
2018-09-04 15:44:35 +0300

    /

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById('c1');
+var ctx =canvas.getContext('2d');
+
+/*Внутри canvas нужно отслеживать движение мыши*/
+canvas.onmousemove = function(event){
+	var x = event.offsetX;
+	var y = event.offsetY;
+	/*В текущий момент времени,когда мышь двигается,в той координате где мышь,
+	отрисовываю прямогульник размером 10 на 10пикселей*/
+	ctx.fillRect(x,y, 10, 10);
+
+
+}
+
+
+
+
+
+
+
